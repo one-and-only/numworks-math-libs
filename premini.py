7edde85f67702ac0b72ca478277b883608466b66
Antonios Papadakis <1001545@isd535.org>
2020-09-17 07:50:22 -0500

    Rename Library File, Remove Python Cache Support

--- premini.py
+++ premini.py
@@ -1,14 +1,14 @@
-import libs.lib1
+import libs.main
 
 prgrmchoice = input("Select From One of the Programs:\n1. Simplify a Fraction (Numerical)\n2. Factor a Quadratic Function\nYour Choice: ")
 if (prgrmchoice == "1"):
     numer = int(input("Enter the Numerator of the Fraction: "))
     denom = int(input("Enter the Denominator of the Fraction: "))
-    libs.lib1.simplify_fraction(numer, denom)
+    libs.main.simplify_fraction(numer, denom)
 elif (prgrmchoice == "2"):
     a = input("What is your a? (y=ax^2+bx+c) ")
     b = input("What is your b? (y=ax^2+bx+c) ")
     c = input("What is your c? (y=ax^2+bx+c) ")
-    libs.lib1.quadratic_function(int(a),int(b),int(c))
+    libs.main.quadratic_function(int(a),int(b),int(c))
 else:
     print("You entered an invalid option.")
